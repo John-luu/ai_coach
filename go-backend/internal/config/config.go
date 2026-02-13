db5144cc0bb380fbd60fcb3b7e8fd9d95e66007b
luolulu <luolulu@3geyue.com>
2026-02-13 23:18:21 +0800

    feat: 修改了数据库

--- go-backend/internal/config/config.go
+++ go-backend/internal/config/config.go
@@ -21,7 +21,7 @@ func Load() *Config {
 		ServerPort: getEnv("AI_COACH_PORT", "8093"),
 		DBUrl:      getEnv("AI_COACH_DB_URL", "tcp(localhost:3306)/ai_coach_db?charset=utf8&parseTime=True&loc=Local"),
 		DBUser:     getEnv("AI_COACH_DB_USER", "root"),
-		DBPass:     getEnv("AI_COACH_DB_PASS", "Root@123456"),
+		DBPass:     getEnv("AI_COACH_DB_PASS", "123456"),
 		AIBaseURL:  getEnv("AI_COACH_AI_BASE_URL", "https://dashscope.aliyuncs.com/compatible-mode/v1"),
 		AIKey:      getEnv("AI_COACH_AI_KEY", "sk-9915ab699b7f46ccbecdea96cdd6c342"),
 		AIModel:    getEnv("AI_COACH_AI_MODEL", "qwen-plus"),
